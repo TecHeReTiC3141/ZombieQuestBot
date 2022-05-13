4340e259bd26eb85eb312694b147a5ee8f99f407
TecHeReTiC <talex73@mail.ru>
2022-05-13 20:39:46 +0500

    Greeting is in progress.

--- client_hadlers.py
+++ client_hadlers.py
@@ -3,16 +3,27 @@ import types
 from bot import *
 
 async def start(message: types.Message):
-
-    cursor.execute('''INSERT INTO User (name, user_id)
+    try:
+        cursor.execute('''INSERT INTO User (name, user_id)
                     VALUES (?, ?)''', (message.from_user.username, message.from_user.id))
+    except Exception as e:
+        print(e)
+
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.row([types.InlineKeyboardButton(text='Да', callback_data='start_quest'),
+    keyboard.row(*[types.InlineKeyboardButton(text='Да', callback_data='start_quest'),
                   types.InlineKeyboardButton(text='Нет', callback_data='miss_quest')])
 
     await message.answer('Приветствую 👋')
     await message.answer('Хочешь начать квест', reply_markup=keyboard)
 
-async def start_quest(message: types.Message):
 
-    await message.reply('Отлично')
+@disp.callback_query_handler(func=lambda c: c.data == 'start_quest')
+async def start_quest(callback_query: types.CallbackQuery):
+
+    await bot.send_message(callback_query.from_user.id, 'Отлично')
+
+
+@disp.callback_query_handler(func=lambda c: c.data == 'miss_quest')
+async def miss_quest(callback_query: types.CallbackQuery):
+
+    await bot.send_message(callback_query.from_user.id, 'Пока')
--- launching.py
+++ launching.py
@@ -1,12 +1,14 @@
 from client_hadlers import *
 from fsm_content import *
 
+
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(start, commands='start')
-    dp.register_message_handler(start_quest, callback='start_quest')
+
 
 async def on_start(_):
     print('Bot is online')
     register_handlers(disp)
+
 
 executor.start_polling(disp, skip_updates=True, on_startup=on_start)
