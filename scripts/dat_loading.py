f25fd664d7793e7c1d153bf00a9151d4ab38fd13
TecHeReTiC <talex73@mail.ru>
2022-05-18 02:00:10 +0500

    Data loading is in progress. Database was slightly changed.

--- scripts/dat_loading.py
+++ scripts/dat_loading.py
@@ -6,10 +6,14 @@ data = pd.ExcelFile('data.xlsx')
 events = pd.read_excel(data, 'Events')
 
 print(events.head())
-
+cursor.execute('''DELETE FROM Event''')
 for row in range(events.shape[0]):
-    print(events.loc[row].values.tolist())
-    cursor.execute('''INSERT INTO Event (event_id, text)
-                    VALUES (?, ?)''', events.loc[row].values.tolist()[:2])
+
+    id, text, im, audio = events.loc[row].values.tolist()
+    try:
+        cursor.execute('''INSERT INTO Event
+                    VALUES (?, ?, ?, ?)''', (int(id), text, im, audio))
+    except Exception as e:
+        print(e, events.loc[row].values.tolist())
 
 db.commit()
