57af376d52ddd1683758bde87232cf9ba6f67c21
TecHeReTiC <talex73@mail.ru>
2022-05-21 21:37:00 +0500

    Creating voices.

--- scripts/creating_audio.py
+++ scripts/creating_audio.py
@@ -2,3 +2,21 @@ import subprocess
 from bot import *
 import gtts
 
+cursor.execute('''SELECT event_id, text
+            FROM Event''')
+
+events = cursor.fetchall()
+
+print(*events, sep='\n')
+for id, text in events:
+    path = f'../audio/wavs/{id}.wav'
+
+    tts = gtts.gTTS(text, lang='ru', )
+    tts.save(path)
+
+    path_conv = f'../audio/voices/{id}.ogg'
+
+    subprocess.run(['../ffmpeg/bin/ffmpeg.exe', '-i', path, path_conv])
+
+
+
